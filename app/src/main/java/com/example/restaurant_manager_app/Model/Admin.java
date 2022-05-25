23df8d4495a924b16a92a4a672a8cf5e4964db7b
Văn Thức <72640345+thucvan@users.noreply.github.com>
2022-05-25 19:29:22 +0700

    thucvan

---
+++ app/src/main/java/com/example/restaurant_manager_app/Model/Admin.java
@@ -0,0 +1,30 @@
+package com.example.restaurant_manager_app.Model;
+
+public class Admin {
+    private String username;
+    private String pass;
+
+    public Admin(String username, String pass) {
+        this.username = username;
+        this.pass = pass;
+    }
+
+    public Admin() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
--- app/src/main/java/com/example/restaurant_manager_app/login_activity.java
+++ app/src/main/java/com/example/restaurant_manager_app/login_activity.java
@@ -2,13 +2,32 @@ package com.example.restaurant_manager_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.restaurant_manager_app.Model.Admin;
 
 public class login_activity extends AppCompatActivity {
-
+    EditText ed_user, ed_pass;
+    Button btn_login;
+    Admin admin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ed_user = findViewById(R.id.ed_username_login);
+        ed_pass = findViewById(R.id.ed_pass_login);
+        btn_login = findViewById(R.id.button_login);
+        btn_login.setOnClickListener(v -> {
+            if(ed_user.getText().toString() == admin.getUsername() && ed_pass.getText().toString() == admin.getPass()){
+                startActivity(new Intent(this, MainActivity.class));
+            }else{
+                Toast.makeText(this , "SomeThing Wrong !!!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
