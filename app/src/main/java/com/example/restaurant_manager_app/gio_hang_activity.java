6c5ff42d4ca7fed65a11e47fefa1f46cebbab875
Văn Thức <72640345+thucvan@users.noreply.github.com>
2022-05-30 17:15:53 +0700

    thucvan

---
+++ app/src/main/java/com/example/restaurant_manager_app/gio_hang_activity.java
@@ -0,0 +1,18 @@
+package com.example.restaurant_manager_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class gio_hang_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gio_hang);
+    }
+
+    public void back(View view) {
+    }
+}
