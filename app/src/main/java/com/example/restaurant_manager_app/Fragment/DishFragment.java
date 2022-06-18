e1ed61f0a64f195e7a50e207b10461a2b40fcbe6
Văn Thức <72640345+thucvan@users.noreply.github.com>
2022-06-18 11:58:22 +0700

    edit_img_suamonan

--- app/src/main/java/com/example/restaurant_manager_app/Fragment/DishFragment.java
+++ app/src/main/java/com/example/restaurant_manager_app/Fragment/DishFragment.java
@@ -1,15 +1,20 @@
 package com.example.restaurant_manager_app.Fragment;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,12 +33,19 @@ import com.example.restaurant_manager_app.Interface.RunSql;
 import com.example.restaurant_manager_app.Model.Dish;
 import com.example.restaurant_manager_app.Model.Order;
 import com.example.restaurant_manager_app.R;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
+import gun0912.tedbottompicker.TedBottomPicker;
 
 
 public class DishFragment extends Fragment implements GetData, RunSql {
@@ -47,7 +59,8 @@ public class DishFragment extends Fragment implements GetData, RunSql {
     SwipeRefreshLayout mySwipeRefreshLayout;
     Dialog dialog;
     EditText edId, edName, edDescribe, edVote, edPrice, edImage;
-    Button btnSave, btnCancel;
+    Button btnSave, btnCancel, btn_choseimg;
+    ImageView img_item;
     Dish item;
 
     @Override
@@ -152,6 +165,16 @@ public class DishFragment extends Fragment implements GetData, RunSql {
         edPrice.setText(item.getPrice());
         edImage.setText(item.getImage());
 
+        img_item = dialog.findViewById(R.id.img_item);
+        btn_choseimg = dialog.findViewById(R.id.btn_chonanh);
+
+        Picasso.get().load(item.getImage())
+                .into(img_item);
+
+        btn_choseimg.setOnClickListener(v -> {
+            requestPermissonChangImage();
+        });
+
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -219,5 +242,42 @@ public class DishFragment extends Fragment implements GetData, RunSql {
         Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show();
     }
 
+    private void requestPermissonChangImage() {
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                openImagePicker();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(getContext(), "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check();
+    }
+
+    private void openImagePicker() {
+        TedBottomPicker.OnImageSelectedListener onImageSelectedListener = new TedBottomPicker.OnImageSelectedListener() {
+            @Override
+            public void onImageSelected(Uri uri) {
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), uri);
+                    img_item.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(getContext())
+                .setOnImageSelectedListener(onImageSelectedListener).create();
+        tedBottomPicker.show(getParentFragmentManager());
+    }
 
 }
